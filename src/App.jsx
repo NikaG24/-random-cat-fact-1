284f0b8eea5759d08c585ed9d4f63acc9658554b
Nathálya Ricci <102884724+nathalya-ricci@users.noreply.github.com>
2023-05-07 19:13:06 -0300

    Add API to generate random cat facts and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-import './App.css'
-import Toggle from 'react-styled-toggle';
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios';
 
 const App = () => {
+  const [catFact, setCatFact] = useState(null);
+  const [catImage, setCatImage] = useState(null);
+
+  const fetchCatFact = () => {
+    axios.get('https://catfact.ninja/fact').then(res => {
+      setCatFact(res.data.fact);
+      fetchCatImage();
+    });
+  };
+
+  const fetchCatImage = () => {
+    axios.get(`https://cataas.com/cat?type=sq${new Date().getTime()}`).then(res => {
+      setCatImage(res.config.url);
+    });
+  };
+
+  const handleClick = () => {
+    fetchCatFact();
+  };
 
   return (
     <div className="App">
-      <Toggle width={40} height={22} sliderWidth={15} sliderHeight={15} translate={15} backgroundColorChecked='#0F0F0F' />
       <h1 className='title'>Welcome to <br /> Random Cat Fact</h1>
       <p className='sub-title'>Click on the button <br /> to generate a cat fact</p>
-      <button>Generate fact</button>
-      {/* <img src="" alt="Random cat image" /> */}
-      <p className="random-fact">This is a random cat fact meow meow meow meow meow and meow meow meow meow</p>
-      {/* TODO: colocar tag image */}
-      <div className="image"></div>
+      <button onClick={handleClick}>Generate fact</button>
+      {catFact !== null && <p className="random-fact">{catFact}</p>}
+      {catImage !== null && <img src={catImage} alt="A random cat image" className='cat-image' />}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
